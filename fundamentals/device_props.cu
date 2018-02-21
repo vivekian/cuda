44817d3a61da1c6482dad33678921089f907469e
vivekian <vivekaseeja@me.com>
2018-02-21 15:05:52 -0800

    get cuda device properties and print them

---
+++ fundamentals/device_props.cu
@@ -0,0 +1,15 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+   cudaDeviceProp prop;
+   cudaGetDeviceProperties(&prop, 0);
+
+   printf("Device name: %s\n", prop.name);
+   printf("Memory clock rate (KHz) : %d\n", prop.memoryClockRate);
+   printf("Memory bus width (bits) : %d\n", prop.memoryBusWidth);
+   printf("Compute capability : %d.%d\n", prop.major, prop.minor);
+   printf("Max threads per block : %d\n", prop.maxThreadsPerBlock);
+   return 0;
+}
